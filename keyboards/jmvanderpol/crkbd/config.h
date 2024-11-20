523e5278ae0324de07225be280cd578ef618512e
Jesse <jvanderpol@thorne.com>
2024-11-20 03:22:14 -0600

    Create jmvanderpol Corne Keyboard using KB2040

---
+++ keyboards/jmvanderpol/crkbd/config.h
@@ -0,0 +1,10 @@
+// Copyright 2024 jesse vanderpol (@jmvanderpol)
+// SPDX-License-Identifier: GPL-2.0-or-later
+#pragma once
+
+#define RP2040_BOOTLOADER_DOUBLE_TAP_RESET
+
+#define EE_HANDS
+#define I2C_DRIVER I2CD1
+#define I2C1_SDA_PIN GP2
+#define I2C1_SCL_PIN GP3
